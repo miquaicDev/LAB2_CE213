3bf91cf116a75248a6508a793a0c8305d0751135
UIT 24520121 <24520121@gm.uit.edu.vn>
2026-04-12 20:45:44 +0700

    Update testbench.v

--- LAB2_Bài_2/testbench.v
+++ LAB2_Bài_2/testbench.v
@@ -2,15 +2,19 @@
 module tb ();
 	parameter width=2048;
 	parameter high=1365;
+	parameter Bright= -50;
 	reg clk,rst_n;
 	reg signed [8:0] bright;
-	reg [383:0] pixel;
+	reg [32759:0] pixel_in;
 	reg [23:0] Ram_input [0:2795519]; 
 	reg [7:0] temp_ram [0:2795519];
-	wire [127:0] out;
-	integer i,j,k;
-	Main_lab2 dut (
-       .clk(clk),.rst_n(rst_n),.pixel(pixel),.bright(bright),.out(out)
+	wire [10919:0] Grayscale_out;
+	integer i,j,k,m;
+	Main_lab2 #(.Bright(Bright))
+	dut
+	(
+       .clk(clk),.rst_n(rst_n),.pixel_in(pixel_in),
+       .Grayscale_out(Grayscale_out)
     );
 	always begin
 		#5 clk=~clk;
@@ -18,22 +22,28 @@ module tb ();
 	initial begin
 		clk=0;
 		$readmemh ("input.txt",Ram_input);
-		bright=0;
 		rst_n=0; 
-		#15;
+		#12;
 		rst_n=1;
-		for(i=0;i<2795520; i=i+16) begin
-		      pixel=0;
-		      for(j=0;j<16;j=j+1) begin
-			     pixel[24*j +: 24]=Ram_input[i+j];
+		for(i=0;i<2795520; i=i+1365) begin
+		      @(negedge clk);
+		      pixel_in=0;
+		      for(j=0;j<1365;j=j+1) begin
+			     pixel_in[24*j +: 24]=Ram_input[i+j];
 			end
-			repeat(4)@(posedge clk);
-			#1;
-			 for (k=0; k<16; k=k+1) begin
-			     temp_ram[i+k]=out[8*k +: 8];
-			 end
 		end
-		$writememh("output.txt",temp_ram);
-		$stop;
 	end
+initial begin
+    wait(rst_n == 1);
+    repeat(5) @(negedge clk);
+    for (m=0; m<2048*1365; m=m+1365) begin
+        #1; 
+        for(k=0; k<1365; k=k+1) begin
+            temp_ram[m+k] = Grayscale_out[8*k +: 8];
+        end
+        @(negedge clk);
+    end
+    $writememh("output.txt", temp_ram);
+    $stop; 
+end
 endmodule
