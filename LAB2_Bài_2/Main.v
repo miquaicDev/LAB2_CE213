29c47aec05c470515ab7c6a50a3a1a30fe07eccb
UIT 24520121 <24520121@gm.uit.edu.vn>
2026-04-12 20:44:02 +0700

    Update Main.v

--- LAB2_Bài_2/Main.v
+++ LAB2_Bài_2/Main.v
@@ -1,15 +1,16 @@
 
-module Main_lab2 (clk,rst_n,pixel,bright,out);
+module Main_lab2 (clk,rst_n,pixel_in,Grayscale_out);
+parameter signed Bright=0;
 parameter width=2048;
 parameter height=1365;
-input [383:0] pixel;
+input [32759:0] pixel_in;
 input clk, rst_n;
-input wire signed [8:0] bright;
-output [127:0] out;
+output [10919:0] Grayscale_out;
 genvar i;
 generate 
-    for (i=0; i<16; i=i+1) begin: rgb_grayscale
-        RGB inst (.clk(clk),.rst_n(rst_n),.data_in(pixel[24*i +: 24]),.Bright(bright),.Y(out[8*i +: 8]));
+    for (i=0; i<height; i=i+1) begin: rgb_grayscale
+        RGB_to_Grayscale #(.Bright(Bright)) inst (.clk(clk),.rst_n(rst_n),
+        .data_in(pixel_in[24*i +: 24]),.pixel_grayscale(Grayscale_out[8*i +: 8]));
     end
 endgenerate 
 endmodule
